5dd855d140a7b40c29eddb1e4d2575542f0e64ce
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-17 09:48:34 +0530

    easy

--- InterviewBit/Seats.cpp
+++ InterviewBit/Seats.cpp
@@ -1,35 +1,43 @@
 #define MOD 10000003
 
-// Source : https://github.com/cruxrebels/InterviewBit/blob/master/Greedy/Seats.cpp
-
 int Solution::seats(string A) {
-    vector<int> pos;
-    for(int i=0; i<A.size(); i++) {
+    int cnt = 0, ret = 0;
+    for(int i = 0; i < A.size(); i++) {
         if(A[i] == 'x')
-            pos.push_back(i);
+            cnt++;
     }
-    // Find median of the x's and hop all close to it
-    if(!pos.size())
-        return 0;
-    int mid = pos.size()/2;
-    int median = pos.size() & 1 ? pos[mid] : (pos[mid - 1] + pos[mid])/2;
-    int empty = A[median] == 'x' ? median -1 : median; 
-    int hops = 0;
-    for(int s = median - 1; s>=0; s--) {
-        if(A[s] == 'x') {
-            hops += (empty - s);
-            hops %= MOD;
-            empty--;
+    int c = 0;
+    int median = 0;
+    for(int i = 0; i < A.size(); i++) {
+        if(A[i] == 'x') {
+            c++;
+            if((cnt / 2 + 1) == c && cnt % 2) {
+                median = i;
+            }
+            if((cnt / 2 == c || (cnt / 2 + 1) == c) && cnt % 2 == 0) {
+                median += i;
+            }
         }
     }
-    empty = median + 1;
-    for(int e = median + 1; e < A.size(); e++) {
-        if(A[e] == 'x') {
-            hops += (e - empty);
-            hops %= MOD;
-            empty++;
+    if(cnt % 2 == 0)
+        median /= 2;
+    int i = median, start = i;
+    while(i >= 0) {
+        if(A[i] == 'x') {
+            ret = ret % MOD + (start % MOD - i % MOD) % MOD;
+            start--;
         }
+        i--;
     }
-    return hops%MOD;
+    i = median + 1;
+    int end = i;
+    while(i < A.size()) {
+        if(A[i] == 'x') {
+            ret = ret % MOD + (i % MOD - end % MOD) % MOD;
+            end++;
+        }
+        i++;
+    }
+    return ret;
 }
 
