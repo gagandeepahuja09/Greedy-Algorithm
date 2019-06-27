2fdf612a223b47818a96a212fd321271af8bebbc
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-27 11:43:01 +0530

    easy

--- InterviewBit/OrderOfPeopleHeights.cpp
+++ InterviewBit/OrderOfPeopleHeights.cpp
@@ -1,16 +1,17 @@
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    return ((a.first > b.first) || (a.first == b.first && a.second < b.second));
+}
+
 vector<int> Solution::order(vector<int> &A, vector<int> &B) {
-    vector<int> ret;
     vector<pair<int, int>> people;
     for(int i = 0; i < A.size(); i++) {
         people.push_back({ A[i], B[i] });
     }
-    sort(people.begin(), people.end(),[](pair<int,int> p1, pair<int,int> p2){
-        return p1.first > p2.first || (p1.first == p2.first && p1.second < p2.second);
-    });
-    vector<int> sol;
-    for (auto person : people){
-        sol.insert(sol.begin() + person.second, person.first);
+    sort(people.begin(), people.end(), cmp);
+    vector<int> ret;
+    for(int i = 0; i < people.size(); i++) {
+        ret.insert(ret.begin() + people[i].second, people[i].first);
     }
-    return sol;
+    return ret;
 }
 
