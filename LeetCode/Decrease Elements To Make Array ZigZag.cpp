b1989975f1b9e7d43dbd38b7c349991557f103fb
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-08-04 10:30:34 +0530

    good question

---
+++ LeetCode/Decrease Elements To Make Array ZigZag.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int movesToMakeZigzag(vector<int>& a) {
+        int c1 = 0, c2 = 0, n = a.size();
+        for(int i = 0; i < n; i++) {
+            int temp = INT_MAX;
+            if(i > 0) {
+                temp = min(temp, a[i - 1]);
+            }
+            if(i < n - 1) {
+                temp = min(temp, a[i + 1]);
+            }
+            if(a[i] >= temp) {
+                if(i % 2 == 0)
+                    c1 += (a[i] - temp + 1);
+                else
+                    c2 += (a[i] - temp + 1);
+            }
+        }
+        return min(c1, c2);
+    }
+};
