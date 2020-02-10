d65de90237f6d1b986fd993c68f3d47dd92246b3
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-10 19:00:25 +0530

    highly IMP revised

--- LeetCode/Car Fleet.cpp
+++ LeetCode/Car Fleet.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int carFleet(int target, vector<int>& pos, vector<int>& sp) {
-        map<int, double> mp;
-        int ret = 0;
-        double curr = 0;
-        for(int i = 0; i < sp.size(); i++) {
-            mp[-pos[i]] = (double)(target - pos[i]) / sp[i];
+    int carFleet(int target, vector<int>& pos, vector<int>& speed) {
+        map<int, double> posToTime;
+        for(int i = 0; i < pos.size(); i++) {
+            posToTime[pos[i]] = ((double)target - pos[i]) / speed[i];
         }
-        for(auto it : mp) {
-            if(it.second > curr)
-                curr = it.second, ret++;
+        int fleets = 0;
+        double currTime = 0;
+        for(auto it = posToTime.rbegin(); it != posToTime.rend(); it++) {
+            if(it -> second > currTime) {
+                currTime = it -> second;
+                ++fleets;
+            }
         }
-        return ret;
+        return fleets;
     }
 };
