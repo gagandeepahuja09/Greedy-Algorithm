743e0377c2db540859e6bec31f0ce49275e77197
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-10 20:36:42 +0530

    highly IMP revised

--- LeetCode/Score After Flipping Matrix.cpp
+++ LeetCode/Score After Flipping Matrix.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     int matrixScore(vector<vector<int>>& A) {
         int n = A.size(), m = A[0].size(), ans = 0;
-        for(int i = 0; i < n; i++) {
-            // Toggling rows by first column, we want to make all
-            // 1 or all 0
-            for(int j = 0; j < m; j++) {
-                A[i][j] ^= A[i][0];    
-            }
-        }
         for(int j = 0; j < m; j++) {
-            int cnt = 0;
+            int count = 0;
             for(int i = 0; i < n; i++) {
-                if(A[i][j] == 0)
-                    cnt++;
+                count += (A[i][j] == A[i][0]);
             }
-            ans += (max(cnt, n - cnt) * (1 << (m - 1 - j)));
+            ans += max(count, n - count) * (1 << (m - j - 1));
         }
-        return ans;                
-    }   
+        return ans;
+    }
 };
